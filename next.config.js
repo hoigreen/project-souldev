a90b2188d92e5bc8f072b27750cf62c4f722d955
Truong Quoc Hoi <hoitrnd107@gmail.com>
2024-02-26 21:45:30 +0700

    chore: config project

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
 module.exports = withNextIntl({
   // Other Next.js configuration ...
+  experimental: {
+    optimizePackageImports: [
+      '@codefixlabs/hooks',
+      '@codefixlabs/lib',
+      '@codefixlabs/ui',
+    ],
+  },
 });
