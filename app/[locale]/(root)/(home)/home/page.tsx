51622f36f0dd136d9fa5394f1e464e83403c2c71
Truong Quoc Hoi <hoitrnd107@gmail.com>
2024-05-07 23:00:27 +0700

    chore: add new resources

--- app/[locale]/(root)/(home)/home/page.tsx
+++ app/[locale]/(root)/(home)/home/page.tsx
@@ -13,5 +13,31 @@ export default function HomePage({
 }) {
   unstableSetRequestLocale(locale);
 
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="head-text text-left">Home</h1>
+
+      {/* <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section> */}
+    </div>
+  );
 }
---
+++ components/app/error-stage.tsx
@@ -0,0 +1,122 @@
+import Link from 'next/link';
+import { twMerge } from 'tailwind-merge';
+import { buttonVariants } from '../ui/button';
+import PageNotFoundImage from '@/public/illustrations/404.svg';
+import ServerErrorImage from '@/public/illustrations/500.svg';
+import UnauthorizedImage from '@/public/illustrations/401.svg';
+
+export enum ErrorStageType {
+  PageNotFound = 'PageNotFound',
+  ServerError = 'ServerError',
+  Unauthorized = 'Unauthorized',
+  ResourceNotFound = 'ResourceNotFound',
+}
+
+type ErrorStageTypeMap = {
+  [key in ErrorStageType]: {
+    title: string;
+    description: string;
+    action: string;
+    href: string;
+    image: React.ReactNode;
+  };
+};
+
+const errorStageTypeMap: ErrorStageTypeMap = {
+  [ErrorStageType.ResourceNotFound]: {
+    action: 'Go back to home',
+    description: 'The resource you are looking for does not exist.',
+    href: '/',
+    image: (
+      <PageNotFoundImage
+        className="aspect-[282/219] h-auto max-w-full"
+        height="219"
+        width="282"
+      />
+    ),
+    title: 'Resource not found',
+  },
+  [ErrorStageType.PageNotFound]: {
+    action: 'Go back to home',
+    description: 'The page you are looking for does not exist.',
+    href: '/',
+    image: (
+      <PageNotFoundImage
+        className="aspect-[282/219] h-auto max-w-full"
+        height="219"
+        width="282"
+      />
+    ),
+    title: 'Page not found',
+  },
+  [ErrorStageType.ServerError]: {
+    action: 'Go back to home',
+    description:
+      'The server has been deserted for a while. Try to refresh this page or feel free to contact us if the problem persists.',
+    href: '/',
+    image: (
+      <ServerErrorImage
+        className="aspect-[275/240] h-auto max-w-full"
+        height="240"
+        width="275"
+      />
+    ),
+    title: 'Internal Server Error',
+  },
+  [ErrorStageType.Unauthorized]: {
+    action: 'Go back to home',
+    description: 'You are not authorized to access this page.',
+    href: '/',
+    image: (
+      <UnauthorizedImage
+        className="aspect-[284/245] h-auto max-w-full"
+        height="245"
+        width="284"
+      />
+    ),
+    title: 'Unauthorized',
+  },
+};
+
+export function ErrorStage({
+  data,
+  stage,
+  className,
+}: {
+  className?: string;
+  data?: unknown;
+  stage: ErrorStageType;
+}): React.JSX.Element {
+  const { title, description, action, href, image } = errorStageTypeMap[stage];
+
+  return (
+    <div
+      className={twMerge(
+        'flex h-full flex-col items-center justify-center px-4 py-12',
+        className,
+      )}
+    >
+      {image}
+
+      <div className="mt-5 text-center">
+        <h1 className="text-2xl font-bold capitalize">{title}</h1>
+        <p className="mx-auto mt-3 max-w-md text-sm text-gray-500">
+          {description}
+        </p>
+        {data ? (
+          <div className="mt-5 max-w-lg overflow-hidden rounded border bg-neutral-100 text-left">
+            <div className="overflow-auto p-2">
+              <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
+            </div>
+          </div>
+        ) : null}
+        <Link
+          className={twMerge(buttonVariants(), 'mt-7.5 w-full max-w-xs')}
+          href={href}
+        >
+          {action}
+        </Link>
+      </div>
+    </div>
+  );
+}
