1f8dad0644e2418361a2a5a17c23a81ce01ff27c
Truong Quoc Hoi <hoitrnd107@gmail.com>
2024-05-09 22:59:27 +0700

    chore: fix build

--- components/onboarding/section/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@/components/employer/onboarding/section/step1';
-export * from '@/components/employer/onboarding/section/step2';
-export * from '@/components/employer/onboarding/section/welcome';
