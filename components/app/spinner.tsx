2bc4e9b5b3ef05b6c0c0f3ac7d9566ba8ee02c70
Truong Quoc Hoi <hoitrnd107@gmail.com>
2024-05-22 22:52:39 +0700

    refactor: Update Spinner component to fix dark mode text color

--- components/app/spinner.tsx
+++ components/app/spinner.tsx
@@ -8,7 +8,7 @@ export function Spinner({
 }): React.JSX.Element {
   return (
     <div className={className}>
-      <Loader2Icon className="animate-spin text-neutral-600 dark:bg-neutral-200" />
+      <Loader2Icon className="animate-spin text-neutral-600 dark:text-neutral-200" />
     </div>
   );
 }
